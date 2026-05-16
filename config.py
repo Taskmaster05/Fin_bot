1412798a5212ce13fa2e19a9e9171c6bd6d0f484
Taskmaster05 <bhagatvibhav2005@gmail.com>
2026-05-16 22:04:01 +0530

    feat: project scaffold, folder structure, requirements, config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NEO4J_URI      = os.getenv("NEO4J_URI")
+NEO4J_USER     = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")

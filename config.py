1ff64962b0b42112c316dcf5234af1b0e3eef9a3
Taskmaster05 <bhagatvibhav2005@gmail.com>
2026-05-17 18:12:39 +0530

    Minor changes

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NEO4J_URI      = os.getenv("NEO4J_URI")
-NEO4J_USER     = os.getenv("NEO4J_USER")
+NEO4J_USERNAME     = os.getenv("NEO4J_USERNAME")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_INDEX = os.getenv("PINECONE_INDEX_NAME")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
